0087c02b5c15d12c097fc609c4184c78da767652
MagnunAVF <magnunavf@gmail.com>
2023-09-05 23:49:46 -0300

    Reduce the bullet size

--- main.lua
+++ main.lua
@@ -55,7 +55,7 @@ function love.draw()
     end
 
     for _,b in ipairs(bullets) do
-        love.graphics.draw(sprites.bullet, b.x, b.y)
+        love.graphics.draw(sprites.bullet, b.x, b.y, nil, 0.5, nil, sprites.bullet:getWidth() / 2, sprites.bullet:getHeight() / 2)
     end
 end
 
